9e71a448ce69f671a5e11cc731c18af515c23936
Javier Pérez <j.perez.de.pedro@accenture.com>
2020-10-08 18:50:58 +0200

    Revert changes on VUE project and update docs/ folder

--- main.js
+++ main.js
@@ -1,57 +1,11 @@
-"use strict";
+import Vue from 'vue'
+import App from './App.vue'
 
-	Vue.directive("auto-focus", {
-		bind: function () {
-			Vue.nextTick(function () {
-				this.el.focus();
-			}.bind(this));
-		}
-	});
+Vue.config.productionTip = false
 
-	new Vue({
-		
-		el: "#todoList",
-		data: {
-			newTask: "",
-			tasks: [
-				{
-					text: "Learn Vue",
-					checked: true
-        },
-        {
-					text: "Buy toilet paper",
-					checked: false
-        },
-        {
-					text: "Do something awesome",
-					checked: false
-				}
-			]
 
-		},
-		
-		methods: {
 
-			addTask: function () {
-				var task = this.newTask.trim();
-				if (task) {
-					this.tasks.push({text: task, checked: false});
-					this.newTask = "";
-				}
-			},
-
-			removeTask: function (task) {
-        var index = this.tasks.indexOf(task);
-				this.tasks.splice(index, 1);
-			},
-
-			check: function (task) {
-				task.checked = true;
-			},
-
-			isChecked: function (task) {
-				return task.checked;
-			}
-
-		}
-	});
+new Vue({
+  render: h => h(App),
+  
+}).$mount('#app')
